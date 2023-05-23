79012810fbe20ee8cfb91ec1bdbc18d25bd7c704
qdrs <qdrs@proton.me>
2023-05-23 13:41:50 +0800

    major refactor to cli

--- src/blockchains.rs
+++ src/blockchains.rs
@@ -1,14 +1,9 @@
-use std::fmt::Display;
-
-use inquire::{
-    error::InquireResult, list_option::ListOption, validator::Validation, MultiSelect, Select, Text,
-};
 use serde::{Deserialize, Serialize};
-use strum::{Display, EnumIter, IntoEnumIterator};
+use serde_json::json;
+use strum::Display;
 
-pub mod solana;
-
-#[derive(Debug, Clone, Copy, EnumIter, Serialize, Deserialize, Display)]
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, Display)]
+#[strum(serialize_all = "snake_case")]
 #[serde(rename_all = "snake_case")]
 pub enum Blockchain {
     Solana,
@@ -16,53 +11,30 @@ pub enum Blockchain {
     Ethereum,
 }
 
-pub(crate) fn select_blockchain() -> InquireResult<Blockchain> {
-    Select::new("Blockchain:", Blockchain::iter().collect()).prompt()
+pub trait BlockchainTaskCommand {
+    fn get_blockchain() -> Blockchain;
 }
 
-fn collect_commands<C>(cmds: Vec<Box<dyn FnOnce() -> Option<C>>>) -> Vec<C> {
-    cmds.into_iter().filter_map(|c| c()).collect()
+#[derive(Debug, Display, clap::ValueEnum, Clone, Serialize, Deserialize)]
+#[strum(serialize_all = "snake_case")]
+#[serde(rename_all = "snake_case")]
+pub enum SolanaTaskCommand {
+    FormalVerification,
 }
 
-impl Blockchain {
-    pub fn select_repo_builder_commands(&self) -> Vec<serde_json::Value> {
-        match self {
-            Blockchain::Solana => collect_commands(solana::generate_repo_commands()),
-            Blockchain::Aptos => todo!(),
-            Blockchain::Ethereum => todo!(),
-        }
-    }
-
-    pub fn get_task_command_list(&self) -> Vec<serde_json::Value> {
-        match self {
-            Blockchain::Solana => solana::get_task_command_list(),
-            Blockchain::Aptos => todo!(),
-            Blockchain::Ethereum => todo!(),
-        }
+impl BlockchainTaskCommand for SolanaTaskCommand {
+    fn get_blockchain() -> Blockchain {
+        Blockchain::Solana
     }
 }
 
-fn prompt_for_field(name: &str) -> Option<String> {
-    Text::new(&format!("Enter {}", name))
-        .with_help_message("If none press enter")
-        .prompt_skippable()
-        .unwrap()
-        .filter(|i| !i.is_empty())
-}
-
-fn prompt_for_multiselect<T: Display>(text: &str, options: Vec<T>) -> InquireResult<Vec<T>> {
-    let validator = &|a: &[ListOption<&T>]| {
-        if a.is_empty() {
-            Ok(Validation::Invalid(
-                "Please select at least one task.".into(),
-            ))
-        } else {
-            Ok(Validation::Valid)
-        }
-    };
-
-    let selected_tasks = MultiSelect::new(text, options)
-        .with_validator(validator)
-        .prompt();
-    selected_tasks
+pub fn get_task_command_list_from_vec<T>(tasks: Vec<T>) -> Vec<serde_json::Value>
+where
+    T: BlockchainTaskCommand + Serialize,
+{
+    let blockchain_name = T::get_blockchain().to_string();
+    tasks
+        .into_iter()
+        .map(|s| json!({"blockchain": blockchain_name, "data": s}))
+        .collect()
 }
